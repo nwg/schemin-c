4dece8e42fcb18b40308216c1197ca247b71b9ca
Nathaniel Griswold <nategriswold@gmail.com>
2020-01-02 00:03:27 -0600

    Run formatter on a func

--- src/prettyprint.c
+++ src/prettyprint.c
@@ -1,76 +1,70 @@
 #include "prettyprint.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "memory.h"
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static char *new_str_with_word_replaced(const char *s, const char *old, 
-								const char *new) 
-{ 
-	char *result;
-	size_t i, cnt = 0;
-	size_t newlen = strlen(new);
-	size_t oldlen = strlen(old);
+static char *new_str_with_word_replaced(const char *s, const char *old,
+                                        const char *new) {
+  char *result;
+  size_t i, cnt = 0;
+  size_t newlen = strlen(new);
+  size_t oldlen = strlen(old);
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (strstr(&s[i], old) == &s[i])
-		{
-			cnt++;
+  for (i = 0; s[i] != '\0'; i++) {
+    if (strstr(&s[i], old) == &s[i]) {
+      cnt++;
 
-			i += oldlen - 1;
-		}
-	}
+      i += oldlen - 1;
+    }
+  }
 
-	result = (char *)malloc(i + cnt * (newlen - oldlen) + 1);
+  result = (char *)malloc(i + cnt * (newlen - oldlen) + 1);
 
-	i = 0;
-	while (*s)
-	{
-		if (strstr(s, old) == s)
-		{
-			strcpy(&result[i], new);
-			i += newlen;
-			s += oldlen;
-		}
-		else
-			result[i++] = *s++;
-	}
+  i = 0;
+  while (*s) {
+    if (strstr(s, old) == s) {
+      strcpy(&result[i], new);
+      i += newlen;
+      s += oldlen;
+    } else {
+      result[i++] = *s++;
+    }
+  }
 
-	result[i] = '\0';
-	return result;
-} 
+  result[i] = '\0';
+  return result;
+}
 
 static void print_cons(object_t *cons);
 
 void print_object(object_t *object) {
   switch (object->type) {
-    case SCHEME_CONS: {
-      print_cons(object);
-      break;
-    }
-    case SCHEME_SYMBOL: {
-      symbol_entry_t *entry = get_symbol_entry(object);
-      printf("%s", entry->sym);
-      break;
-    }
-    case SCHEME_NULL: {
-      assert(object == g_scheme_null && "reused null");
-      printf("'()");
-      break;
-    }
-    case SCHEME_STRING: {
-      string_entry_t *entry = get_string_entry(object);
-      const char *escaped = new_str_with_word_replaced(entry->str, "\"", "\\\"");
-      printf("\"%s\"", escaped);
-      break;
-    }
-    case SCHEME_NUMBER:
-    {
-      printf("<not handled>");
-      break;
-    }
+  case SCHEME_CONS: {
+    print_cons(object);
+    break;
+  }
+  case SCHEME_SYMBOL: {
+    symbol_entry_t *entry = get_symbol_entry(object);
+    printf("%s", entry->sym);
+    break;
+  }
+  case SCHEME_NULL: {
+    assert(object == g_scheme_null && "reused null");
+    printf("'()");
+    break;
+  }
+  case SCHEME_STRING: {
+    string_entry_t *entry = get_string_entry(object);
+    const char *escaped = new_str_with_word_replaced(entry->str, "\"", "\\\"");
+    printf("\"%s\"", escaped);
+    break;
+  }
+  case SCHEME_NUMBER: {
+    printf("<not handled>");
+    break;
+  }
   }
 }
 
